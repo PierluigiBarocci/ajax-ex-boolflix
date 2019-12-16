0fbebdec3c78d9c4ab02734ec988e23b9852813e
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-12-16 16:11:37 +0100

    Iniziamo a dare una bella ripulita al codice per poi dedicarci al tasto Invio

--- main.js
+++ main.js
@@ -13,44 +13,56 @@ var template_function = Handlebars.compile(template_html);
 // Cambiare il valore della query sfruttando il val al click del bottone
 // dobbiamo intercettare il click sull'icona
 $('#search_icon').click(function(){
+    // come prima cosa bisogna svuotare il container, altrimenti si accavallano ricerche su ricerche
+    $('.mainview.container').find('ul').remove();
     //prendere il valore dell'input,
     var user_research = $('.searchbar > input').val();
-    console.log(user_research);
     // dobbiamo darlo alla query della chiamata ajax
+    // ma prima che parta la chiamata ajax, se il valore è nullo, blocchiamo subito
+    if (user_research.length < 3) {
+        alert('Mi dispiace, ma per iniziare la ricerca servono almeno 3 caratteri');
+    } else {
+
+    };
+
+
+})
+// aggiunger il click del tasto invio
+
+
+// FUNCTIONS
+
+function myCalling() {
     $.ajax ({
         'url': api_themov + 'search/movie',
-        // come visto stamattina, usiamo data per riordinare un po'il codice e rendere tutto più accessibile e leggibile;
+
         'data': {
             'api_key': '82d42d7ba19cc3f165f25f52f34da589',
-            // è qui che dobbiamo intervenire per interagire con l'input
             'query': user_research,
             'language': 'it-IT'
         },
         'method': 'GET',
         'success': function(data){
             var movies = data.results;
-            console.log(movies);
-            for (var i = 0; i < movies.length; i++) {
-                // intercettiamo ogni singolo film
-                var film = movies[i];
-                // e di esso ci servono Titolo, Titolo Originale, Lingua e Voto
-                // per adesso creaimo un oggetto con le varibili che ci servono
-                var object = {
-                    'title': film.title,
-                    'ori_title': film.original_title,
-                    'language': film.original_language,
-                    'vote': film.vote_count
-                };
-                // diamo l'oggeto alla funzione Handlebars
-                var final = template_function(object);
-                // lo appendiamo al container nel main
-                $('.mainview.container').append(final);
-            }
+            if (movies.length == 0) {
+                alert('Mi dispiace, non ho trovato niente, riprova');
+            } else {
+                for (var i = 0; i < movies.length; i++) {
+                    var film = movies[i];
+                    var object = {
+                        'title': film.title,
+                        'ori_title': film.original_title,
+                        'language': film.original_language,
+                        'vote': film.vote_average
+                    };
+                    var final = template_function(object);
+                    $('.mainview.container').append(final);
+                }
+            };
+            $('.searchbar > input').val('');
         },
         'error': function(){
             alert('error');
         }
     });
-
-})
-// aggiunger il click del tasto invio
+};
