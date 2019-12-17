96734b832e916ca82c2e00cd93331afa51e33516
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-12-17 16:24:53 +0100

    Milestone 4 Terminata, abbastanza soddisfatto, ma vorrei dividere il layout in Serie e Film, ci tornerò alla fine

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo
-// scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il
-// bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni
-// film trovato:
-// 1. Titolo
-// 2. Titolo Originale
-// 3. Lingua
-// 4. Voto
 
 //Click Search Icon
 $('#search_icon').click(function(){
@@ -37,27 +27,15 @@ $('.searchbar > input').keypress(function(event){
     }
 });
 
-// Milestone 2:
-// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
-// permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
-// lasciando le restanti vuote (troviamo le icone in FontAwesome).
-// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze
-// piene (o mezze vuote :P)
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della
-// nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della
-// nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-
-// star :
-// <i class="fas fa-star"></i>
-// empty star:
-// <i class="far fa-star"></i>
-
-// Math.ceil(4,1) --> expected result: 5;
-// var prov_vote = (object.vote_average / 2);
-// var stars_vote = Math.ceil(prov_vote);
-
-
-
+// when I go over the card
+// I want to show off the properites of the movie
+// I have to use on document for the template
+$(document).on('mouseover', '.card', function(){
+    $(this).find('.properties').fadeIn();
+})
+$(document).on('mouseleave', '.card', function(){
+    $('.properties').fadeOut();
+});
 
 
 // FUNCTIONS
@@ -147,12 +125,15 @@ function cardGenerator(object) {
         };
         if (object.poster_path == null) {
             var properties = {
+                // if there aren't posters I get a standard image
                 'background': 'images/no_poster.jpg',
                 'title': object.name || object.title,
                 'ori_title': object.original_name || object.original_title,
                 'language': flag_lang,
                 'vote': '<i class="fas fa-star"></i>'.repeat(stars_vote),
                 'no_vote': '<i class="far fa-star"></i>'.repeat(5 - stars_vote),
+                // I want to slice the overview at 300 char
+                'overview': (object.overview).slice(0, 300) + '...'
             };
         } else{
             var properties = {
@@ -162,8 +143,9 @@ function cardGenerator(object) {
                 'language': flag_lang,
                 'vote': '<i class="fas fa-star"></i>'.repeat(stars_vote),
                 'no_vote': '<i class="far fa-star"></i>'.repeat(5 - stars_vote),
+                'overview': (object.overview).slice(0, 300) + '...'
             };
-        } 
+        }
         var final = template_function(properties);
         $('.mainview.container').append(final);
 }
